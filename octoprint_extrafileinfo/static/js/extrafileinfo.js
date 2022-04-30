34b2c5d24d6a75eb3cedb85d00d5e2f96ed8db91
Lars Wolter <larswolter@me.com>
2022-04-30 16:56:00 +0200

    Added units, fixed implementation and prevent errors

--- octoprint_extrafileinfo/static/js/extrafileinfo.js
+++ octoprint_extrafileinfo/static/js/extrafileinfo.js
@@ -10,24 +10,33 @@ $(function() {
 
         self.filesViewModel = parameters[0];
         
-        const visibleKeys = ["Layer Height", "Nozzle Diameter", "Material Diameter"];
+        //TODO add this as a setting instead
+        // Defined as [[key0, key1, ...], [unit1, unit2, ...]]
+        const visibleKeys = [["Layer Height", "Nozzle Diameter", "Infill Density", "Material Name"], [" mm", " mm", "%", ""]];
         self.filesViewModel.ExtraFileInfo_getInfo = function(data) {
             const slicerSettings = data.slicer_settings;
+            if (slicerSettings === undefined) {
+                return "Reload for more info<br>";
+            }
             let returnStr = "";
-            visibleKeys.forEach((element) => {
-                if (slicerSettings[element] === undefined) {
-                    return;
+            for (let i = 0; i < visibleKeys[0].length; i++) {
+                if (slicerSettings[visibleKeys[0][i]] === undefined) {
+                    continue;
                 }
-
-                returnStr = returnStr + element + ": " + slicerSettings[element] + "<br>";
-            });
+                returnStr = returnStr + visibleKeys[0][i] + ": " + slicerSettings[visibleKeys[0][i]] + visibleKeys[1][i] + "<br>";
+            };
             return returnStr;
         };
 
         // Hijack the getAdditionalData function and add the custom data
         let oldGetData = self.filesViewModel.getAdditionalData;
         self.filesViewModel.getAdditionalData = function(data) {
-            return "" + oldGetData(data) + self.filesViewModel.ExtraFileInfo_getInfo(data);
+            let returnStr = "" + oldGetData(data);
+            if (!returnStr.endsWith('<br>')) {
+                returnStr = returnStr + "<br>";
+            }
+            returnStr = returnStr + self.filesViewModel.ExtraFileInfo_getInfo(data);
+            return returnStr;
         }
     }
 
